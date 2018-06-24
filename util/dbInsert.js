9cfefcca435d87cec4530b5c38bd4477969738c6
renatodeyvson <renatodeyvson@gmail.com>
2018-06-24 15:00:57 +0000

    dbInsert creating users

--- util/dbInsert.js
+++ util/dbInsert.js
@@ -1,5 +1,6 @@
 const mongoClient = require('mongodb').MongoClient,
-    fs = require('fs');
+    fs = require('fs'),
+    randomName = require('node-random-name');
 
 const dbUrl = 'mongodb://localhost:27017/',
     movies = JSON.parse(fs.readFileSync(__dirname + '/../data/movies.json', 'utf8')),
@@ -19,11 +20,29 @@ mongoClient.connect(dbUrl, (err, db) => {
     
             dbo.collection('ratings').insertMany(ratings, (err, result) => {
                 if(err) throw err;
-        
+                
                 dbo.collection('tags').insertMany(tags, (err, result) => {
                     if(err) throw err;
-            
-                    db.close();
+
+                    let users = [],
+                        userRatings = [];
+                    for(let i=0; i< ratings.length; ++i){
+                        if(ratings[i+1] != undefined && ratings[i+1].userId == ratings[i].userId){
+                            userRatings.push({ movieId: ratings[i].movieId, rating: ratings[i].rating });
+                        } else {
+                            users.push({
+                                userId: ratings[i].userId,
+                                name: randomName(),
+                                ratings: userRatings
+                            });
+                            userRatings = [];
+                        }
+                    }
+                    dbo.collection('users').insertMany(users, (err, result) => {
+                        if(err) throw err;
+
+                        db.close();
+                    });
                 });
             });
         });
