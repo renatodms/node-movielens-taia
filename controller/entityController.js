9e2b7cf80eadc6533d4982bbba0fbbc9240c29ee
renatodeyvson <renatodeyvson@gmail.com>
2018-06-24 12:08:48 +0000

    add getAllSized

--- controller/entityController.js
+++ controller/entityController.js
@@ -19,3 +19,22 @@ exports.getAll = (req, res) => {
     });
 
 };
+
+exports.getAllSized = (req, res) => {
+    const entity = req.params.entity,
+        size = req.params.size;
+
+    mongoClient.connect(dbUrl, (err, db) => {
+        if(err) throw err;
+    
+        let dbo = db.db('admin');
+        dbo.collection(entity).find({}).limit(parseInt(size)).toArray((err, result) => {
+            if(err) throw err;
+    
+            res.send(result);
+    
+            db.close();
+        });
+    });
+
+};
--- route.js
+++ route.js
@@ -3,5 +3,6 @@ const entity = require('./controller/entityController');
 module.exports = (app) => {
     
     app.get('/:entity', entity.getAll);
+    app.get('/:entity/:size', entity.getAllSized);
 
 };
