ea3ab15bbd897e18ecd3feb8a145c33ae1e999dc
renatodeyvson <renatodeyvson@gmail.com>
2018-06-24 23:07:33 +0000

    create collection before drop

--- util/dbInsert.js
+++ util/dbInsert.js
@@ -12,51 +12,71 @@ mongoClient.connect(dbUrl, (err, db) => {
     if(err) throw err;
 
     let dbo = db.db('admin');
-    dbo.collection('movies').drop(function(err, delOK) {
+    dbo.createCollection('movies', function(err, res) {
         if (err) throw err;
         
-        dbo.collection('links').drop(function(err, delOK) {
+        dbo.createCollection('links', function(err, res) {
             if (err) throw err;
             
-            dbo.collection('ratings').drop(function(err, delOK) {
+            dbo.createCollection('ratings', function(err, res) {
                 if (err) throw err;
                 
-                dbo.collection('tags').drop(function(err, delOK) {
+                dbo.createCollection('tags', function(err, res) {
                     if (err) throw err;
                     
-                    dbo.collection('users').drop(function(err, delOK) {
+                    dbo.createCollection('users', function(err, res) {
                         if (err) throw err;
                         
-                        dbo.collection('movies').insertMany(movies, (err, result) => {
-                            if(err) throw err;
-                    
-                            dbo.collection('links').insertMany(links, (err, result) => {
-                                if(err) throw err;
-                        
-                                dbo.collection('ratings').insertMany(ratings, (err, result) => {
-                                    if(err) throw err;
+                        dbo.collection('movies').drop(function(err, delOK) {
+                            if (err) throw err;
+                            
+                            dbo.collection('links').drop(function(err, delOK) {
+                                if (err) throw err;
+                                
+                                dbo.collection('ratings').drop(function(err, delOK) {
+                                    if (err) throw err;
                                     
-                                    dbo.collection('tags').insertMany(tags, (err, result) => {
-                                        if(err) throw err;
-                    
-                                        let users = [],
-                                            userRatings = [];
-                                        for(let i=0; i< ratings.length; ++i){
-                                            if(ratings[i+1] != undefined && ratings[i+1].userId == ratings[i].userId){
-                                                userRatings.push({ movieId: ratings[i].movieId, rating: ratings[i].rating });
-                                            } else {
-                                                users.push({
-                                                    userId: ratings[i].userId,
-                                                    name: randomName(),
-                                                    ratings: userRatings
+                                    dbo.collection('tags').drop(function(err, delOK) {
+                                        if (err) throw err;
+                                        
+                                        dbo.collection('users').drop(function(err, delOK) {
+                                            if (err) throw err;
+                                            
+                                            dbo.collection('movies').insertMany(movies, (err, result) => {
+                                                if(err) throw err;
+                                        
+                                                dbo.collection('links').insertMany(links, (err, result) => {
+                                                    if(err) throw err;
+                                            
+                                                    dbo.collection('ratings').insertMany(ratings, (err, result) => {
+                                                        if(err) throw err;
+                                                        
+                                                        dbo.collection('tags').insertMany(tags, (err, result) => {
+                                                            if(err) throw err;
+                                        
+                                                            let users = [],
+                                                                userRatings = [];
+                                                            for(let i=0; i< ratings.length; ++i){
+                                                                if(ratings[i+1] != undefined && ratings[i+1].userId == ratings[i].userId){
+                                                                    userRatings.push({ movieId: ratings[i].movieId, rating: ratings[i].rating });
+                                                                } else {
+                                                                    users.push({
+                                                                        userId: ratings[i].userId,
+                                                                        name: randomName(),
+                                                                        ratings: userRatings
+                                                                    });
+                                                                    userRatings = [];
+                                                                }
+                                                            }
+                                                            dbo.collection('users').insertMany(users, (err, result) => {
+                                                                if(err) throw err;
+                                        
+                                                                db.close();
+                                                            });
+                                                        });
+                                                    });
                                                 });
-                                                userRatings = [];
-                                            }
-                                        }
-                                        dbo.collection('users').insertMany(users, (err, result) => {
-                                            if(err) throw err;
-                    
-                                            db.close();
+                                            });
                                         });
                                     });
                                 });
